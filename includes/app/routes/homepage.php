7597a763b306590923a30b8f81d439e21f66ef23
Jamie Whittaker <33399796+jamiewhittaker@users.noreply.github.com>
2020-05-01 14:49:36 +0100

    Added featured recipes section to homepage

--- includes/app/routes/homepage.php
+++ includes/app/routes/homepage.php
@@ -3,27 +3,38 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use App\controllers\RecipeDatabaseWrapper;
+
+
 
 
 $app->get('/', function(Request $request, Response $response)
 {
+    $db = new RecipeDatabaseWrapper();
+    $featuredResult = $db->getFeatured();
+    $arr["featured"] = $featuredResult;
+
     if ($_SESSION["loggedIn"] === TRUE) {
         $arr["firstName"] = $_SESSION["firstName"];
         return $this->view->render($response, 'homepage-loggedin.html.twig', $arr);
     } else {
         session_destroy();
-        return $this->view->render($response, 'homepage-loggedout.html.twig');
+        return $this->view->render($response, 'homepage-loggedout.html.twig', $arr);
     }
 })->setName('/' );
 
 $app->get('/homepage', function(Request $request, Response $response)
 {
+    $db = new RecipeDatabaseWrapper();
+    $featuredResult = $db->getFeatured();
+    $arr["featured"] = $featuredResult;
+
     if ($_SESSION["loggedIn"] === TRUE) {
         $arr["firstName"] = $_SESSION["firstName"];
         return $this->view->render($response, 'homepage-loggedin.html.twig', $arr);
 
     } else {
         session_destroy();
-        return $this->view->render($response, 'homepage-loggedout.html.twig');
+        return $this->view->render($response, 'homepage-loggedout.html.twig', $arr);
     }
 })->setName('/homepage' );
